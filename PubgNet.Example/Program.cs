bd1547a64ae199dc44f644d39909ad8d2ad6559d
Sebastian Dang <s1dang@ucsd.edu>
2019-06-29 11:18:19 -0700

    Add Example.

---
+++ PubgNet.Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace PubgNet.Example
+{
+    class Program
+    {
+        static void Main() => new Program().MainAsync().GetAwaiter().GetResult();
+        public async Task MainAsync()
+        {
+            PubgNetClient client = new PubgNetClient("your-api-key");
+            var players = await client.GetPlayersByUsernames(new string[]{"shroud"});
+        }
+    }
+}
